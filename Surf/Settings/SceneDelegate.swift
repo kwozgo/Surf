94c1fc6937207d98ce5d21c59e1ca812b192f98d
Charlie Walton <kwozgo@gmail.com>
2023-02-10 21:09:40 +0300

    Add custom modal view with stages

--- Surf/Settings/SceneDelegate.swift
+++ Surf/Settings/SceneDelegate.swift
@@ -8,10 +8,21 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        let rootViewController = TableViewController()
+//        window = UIWindow(windowScene: windowScene)
+//        window?.rootViewController = rootViewController
+//        window?.makeKeyAndVisible()
+
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let rootViewController = TableViewController()
-        window = UIWindow(windowScene: windowScene)
+        self.window = UIWindow(windowScene: windowScene)
+        let modalViewController = ModalViewController()
+        let rootViewController = UINavigationController()
+        let backgroundImage = UIImage(named: "Background")!
+        rootViewController.view = UIImageView(image: backgroundImage)
+        modalViewController.modalPresentationStyle = .overCurrentContext
         window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
+        rootViewController.present(modalViewController, animated: true)
     }
 }
